402473df2be1e5ceb44f78aaffe1225feee0e430
iddav11 <145371140+iddav11@users.noreply.github.com>
2024-05-02 14:39:22 +0100

    Scene now Spawns (1) godotblock through code

--- Starter Code/Scenes/ButtonPress.gd
+++ Starter Code/Scenes/ButtonPress.gd
@@ -1,16 +1,7 @@
 extends Button
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
-func _on_button_down():
-	$AudioStreamPlayer2D.play()
-	pass # Replace with function body.
+#@onready var godotblock2d = get_node("GodotBlock")
+#
+##func _on_button_down():
+	#godotblock2d.new()
+	#pass # Replace with function body.
--- Starter Code/Scripts/InstantiateScript.gd
+++ Starter Code/Scripts/InstantiateScript.gd
@@ -1,18 +1,22 @@
 extends Node
 var array = [1, 2, 3]
 var GodotBlock
+@export var GodotBlockPrefab:PackedScene
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_tree().get_root().add_child(GodotBlock) 
+	var scene = preload("res://Scenes/GodotBlock.tscn")
+	var instance = scene.instantiate()
+	add_child(instance)
+	#get_tree().get_root().get_scene("GodotBlock") 
+	#get_scene_instance_load_placeholder()
+	
+	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 	
-
-func _on_button_down():
-	pass
 
 
