4d1ee9717464d6709e1689169bd393426270fec0
iddav11 <145371140+iddav11@users.noreply.github.com>
2024-05-02 13:05:34 +0100

    Area Enter code for when blocks land on ground
    It only prints for now

---
+++ Starter Code/Scripts/InstantiateScript.gd
@@ -0,0 +1,18 @@
+extends Node
+var array = [1, 2, 3]
+var GodotBlock
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	get_tree().get_root().add_child(GodotBlock) 
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+	
+
+func _on_button_down():
+	pass
+
+
