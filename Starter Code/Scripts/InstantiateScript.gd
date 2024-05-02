bc0e83bb320523b9e40d263b2e1e1f2da04bbd5c
iddav11 <145371140+iddav11@users.noreply.github.com>
2024-05-02 15:12:31 +0100

    Added slider that spawns 10 godot blocks
    The slider ONLY spawns 10 blocks, basically a button

--- Starter Code/Scripts/InstantiateScript.gd
+++ Starter Code/Scripts/InstantiateScript.gd
@@ -1,12 +1,11 @@
 extends Node
+var array2 = [6,3]
 var array = [1, 2, 3, 4]
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#var scene = preload("res://Scenes/GodotBlock.tscn")
-	#var instance = scene.instantiate()
 	for i in range(10):
 		var scene = preload("res://Scenes/GodotBlock.tscn")
 		var instance = scene.instantiate()
@@ -23,3 +22,13 @@ func _ready():
 	
 
 
+
+
+func _on_h_slider_drag_ended(value_changed):
+	for i in range(10):
+		var scene = preload("res://Scenes/GodotBlock.tscn")
+		var instance = scene.instantiate()
+		print(array[0])
+		add_child(instance)
+	
+	pass # Replace with function body.
